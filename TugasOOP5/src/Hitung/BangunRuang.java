cb0c7608d1d665363e2589f09f2f883784827bd1
riooorante <rantetadungrio@gmail.com>
2023-04-11 23:50:52 +0800

    add

---
+++ TugasOOP5/src/Hitung/BangunRuang.java
@@ -0,0 +1,13 @@
+package Hitung;
+
+public class BangunRuang {
+    int tinggi;
+    int panjang;
+    int lebar;
+
+    public BangunRuang(int tinggi, int panjang, int lebar) {
+        this.tinggi = tinggi;
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+}
